d8dee5597fa8864a644a30df4e6ecf8f51bf740c
Hill-Man-1 <hilmaaansf@gmail.com>
2023-07-01 04:47:44 +0700

    all done

--- JS/script.js
+++ JS/script.js
@@ -1,32 +1,32 @@
-var checkbox = document.getElementById("ChangeTheme"); //get the checkbox to a variable
+var checkbox = document.getElementById("ChangeTheme");
 
-//check storage if dark mode was on or off
+
 if (sessionStorage.getItem("mode") == "dark") {
-  darkmode(); //if dark mode was on, run this funtion
+  darkmode(); 
 } else {
-  nodark(); //else run this funtion
+  nodark(); 
 }
 
-//if the checkbox state is changed, run a funtion
+
 checkbox.addEventListener("change", function() {
-  //check if the checkbox is checked or not
     if (checkbox.checked) {
-        darkmode(); //if the checkbox is checked, run this funtion
+        darkmode();
     } else {
-        nodark(); //else run this funtion
+        nodark(); 
     }
 });
 
-//function for checkbox when checkbox is checked
+
 function darkmode() {
-  document.body.classList.add("dark-mode"); //add a class to the body tag
-  checkbox.checked = true; //set checkbox to be checked state
-  sessionStorage.setItem("mode", "dark"); //store a name & value to know that dark mode is on
+  document.body.classList.add("dark-mode"); 
+  checkbox.checked = true; 
+  sessionStorage.setItem("mode", "dark"); 
 }
 
-//function for checkbox when checkbox is not checked
+
 function nodark() {
-  document.body.classList.remove("dark-mode"); //remove added class from body tag
-  checkbox.checked = false; //set checkbox to be unchecked state
-  sessionStorage.setItem("mode", "light"); //store a name & value to know that dark mode is off or light mode is on
+  document.body.classList.remove("dark-mode"); 
+  checkbox.checked = false; 
+  sessionStorage.setItem("mode", "light");
 }
+
